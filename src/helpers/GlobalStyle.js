dd2dd42dde569a9f3c706deadcf7673983d01ab1
Rens <rensvanlaar@icloud.com>
2021-04-01 22:28:28 +0200

    removed background img

--- src/helpers/GlobalStyle.js
+++ src/helpers/GlobalStyle.js
@@ -10,14 +10,10 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: 'Roboto', sans-serif;
-    background: #212529;
+    background: linear-gradient(to bottom right, #7474bf, #348ac7);
     color: #F8F9FA;
     height: 100vh;
     width: 100vw;
-    background: url('https://images.unsplash.com/photo-1502053877922-431b5e61e449?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80');
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
   }
 
   a {
